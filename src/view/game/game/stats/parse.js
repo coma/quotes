688e12073f39f946aef1eb0546bb350bc8307f47
Eduardo García Sanz <eduardo@comakai.com>
2016-09-16 19:03:48 +0200

    improving chart

--- src/view/game/game/stats/parse.js
+++ src/view/game/game/stats/parse.js
@@ -7,17 +7,17 @@ export default ({positions, length}) => {
 
     const points = positions
         .map((time, position) => ({position, time}))
-        .filter(({time}) => time <= from && to <= time)
         .reverse();
 
     const lines = points
+        .filter(({time}) => time <= from && to <= time)
         .map(({position, time}) => `H${ from - time } V${ length - position }`)
         .join(' ');
 
-    const start = points.length ? length - points[0].position : length;
+    const start = points.length ? length - points[0].position - 1 : length;
 
     return {
         viewBox: `0 0 ${ offset } ${ length }`,
-        d      : `M0 ${ start } ${ lines } V${ length } H0`
+        d      : `M0 ${ start } ${ lines } H${ offset } V${ length } H0`
     };
 };
